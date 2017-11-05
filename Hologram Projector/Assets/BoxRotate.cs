1345424aea43af0135416375fa2380c8b418101a
kbowen99 <kurtisbowen99@gmail.com>
2017-11-04 20:33:33 -0500

    Great Beginnings

---
+++ Hologram Projector/Assets/BoxRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.Rotate(new Vector3(0, 1));
+	}
+}
---
+++ Hologram Projector/Assets/ScreenAdjuster.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenAdjuster : MonoBehaviour {
+
+    float adj = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+        int square = Screen.height;
+        Screen.SetResolution(square, square, true);
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            Camera cam = GameObject.Find("Camera North").GetComponent<Camera>();
+            cam.rect = new Rect(cam.rect.x, cam.rect.y-adj, cam.rect.width, cam.rect.height);
+            cam = GameObject.Find("Camera South").GetComponent<Camera>();
+            cam.rect = new Rect(cam.rect.x, cam.rect.y+adj, cam.rect.width, cam.rect.height);
+            cam = GameObject.Find("Camera West").GetComponent<Camera>();
+            cam.rect = new Rect(cam.rect.x+adj, cam.rect.y, cam.rect.width, cam.rect.height);
+            cam = GameObject.Find("Camera East").GetComponent<Camera>();
+            cam.rect = new Rect(cam.rect.x-adj, cam.rect.y, cam.rect.width, cam.rect.height);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Camera cam = GameObject.Find("Camera North").GetComponent<Camera>();
+            cam.rect = new Rect(cam.rect.x, cam.rect.y + adj, cam.rect.width, cam.rect.height);
+            cam = GameObject.Find("Camera South").GetComponent<Camera>();
+            cam.rect = new Rect(cam.rect.x, cam.rect.y - adj, cam.rect.width, cam.rect.height);
+            cam = GameObject.Find("Camera West").GetComponent<Camera>();
+            cam.rect = new Rect(cam.rect.x - adj, cam.rect.y, cam.rect.width, cam.rect.height);
+            cam = GameObject.Find("Camera East").GetComponent<Camera>();
+            cam.rect = new Rect(cam.rect.x + adj, cam.rect.y, cam.rect.width, cam.rect.height);
+        }
+    }
+}
